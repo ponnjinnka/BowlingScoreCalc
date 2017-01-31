5c3ec3895444a933ec7141d8656a454674b260bb
ponnjinnka <penponnnnjinnka@gmail.com>
2017-02-01 01:25:19 +0900

    考え中

---
+++ BowlingScoreCalc/Frame.swift
@@ -0,0 +1,19 @@
+//
+//  Frame.swift
+//  BowlingScoreCalc
+//
+//  Created by ryota on 2017/02/01.
+//  Copyright © 2017年 Ryota Kato. All rights reserved.
+//
+
+import Foundation
+
+class Frame {
+    var totalScore: Int?
+    var frameScore: String?
+    
+    init(frameScore: String) {
+        // frameScoreを受取、frameScoreに格納してtotalScoreを一旦出す
+    }
+    
+}
---
+++ BowlingScoreCalc/ScoreCalc.swift
@@ -0,0 +1,23 @@
+//
+//  ScoreCalc.swift
+//  BowlingScoreCalc
+//
+//  Created by ryota on 2017/02/01.
+//  Copyright © 2017年 Ryota Kato. All rights reserved.
+//
+
+import Foundation
+
+class ScoreCalc {
+    
+    // 合計スコア算出処理
+    func calcTotalScore(_ scoreSheet: String) {
+        // 配列を生成
+        var frameArray: Array<Frame>
+        // 各フレームごとにスコアシートをFrameクラスに設定する
+        
+        //Frameクラスを配列に追加する
+        
+        // スペアとストライクの判定はcounter%2のあまりから判断する
+    }
+}
